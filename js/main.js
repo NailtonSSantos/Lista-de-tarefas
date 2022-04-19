72230b76206368aa7ebf1aacc4b843473522d7cd
nailtonssantos <nltsilva8@gmail.com>
2022-04-18 22:04:43 -0400

    Config de Tarefa Concluída

--- js/main.js
+++ js/main.js
@@ -8,19 +8,25 @@ const Main = {
     },
 
     cacheSelectors: function(){
-        this.checkButtons = document.querySelectorAll('.check')
+        this.$checkButtons = document.querySelectorAll('.check')
     },
 
     bindEvents: function(){
         const self = this
-        this.checkButtons.forEach(function(button){
+        this.$checkButtons.forEach(function(button){
             button.onclick = self.Events.checkButton_click
         })
     },
 
     Events: {
-        checkButton_click: function(){
-            alert('Ok')
+        checkButton_click: function(e){
+            const li = e.target.parentElement
+            const isDone = li.classList.contains('done')
+
+            if (!isDone) {
+                return li.classList.add('done')
+            }
+            li.classList.remove('done')
         }
     }
 }
